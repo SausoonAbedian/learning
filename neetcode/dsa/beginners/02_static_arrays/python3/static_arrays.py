37bab06a839468c409bd87c4021441ed14088aaf
Sausoon Abedian <abedian.sausoon@gmail.com>
2025-03-26 06:39:42 -0700

    Added static_arrays.py for neetcode dsa

---
+++ neetcode/dsa/beginners/02_static_arrays/python3/static_arrays.py
@@ -0,0 +1,23 @@
+# Insert n into arr at the next open position.
+# Length is the number of 'real' values in arr, and capacity
+# is the size (aka memory allocated for the fixed size array).
+def insertEnd(arr, n, length, capacity):
+    pass
+
+# Remove from the last position in the array if the array
+# is not empty (i.e. length is non-zero).
+def removeEnd(arr, length):
+    pass
+
+# Insert n into index i after shifting elements to the right.
+# Assuming i is a valid index and arr is not full.
+def insertMiddle(arr, i, n, length):
+    pass
+
+# Remove value at index i before shifting elements to the left.
+# Assuming i is a valid index.
+def removeMiddle(arr, i, length):
+    pass
+
+def printArr(arr, capacity):
+    pass
